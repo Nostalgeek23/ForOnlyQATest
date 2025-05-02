813f37a8b4ec09fe1d4b8851b75d77dc936b8c19
Eugene <dorick23@gmail.com>
2025-05-02 23:24:41 +0300

    firefox options and yml install

--- src/test/java/com/testsforonly/utils/DriverUtils.java
+++ src/test/java/com/testsforonly/utils/DriverUtils.java
@@ -38,10 +38,12 @@ public class DriverUtils {
     firefoxOptions.addArguments("-disable-gpu");
     firefoxOptions.addArguments("-no-sandbox");
     firefoxOptions.addArguments("-disable-dev-shm-usage");
-//    firefoxOptions.addArguments("--window-size=1920,1080" );
+    firefoxOptions.addArguments("--window-size=1920,1080");
+    firefoxOptions.addArguments("--profile", "/tmp/firefox-profile");
     firefoxOptions.setCapability("se:recordVideo", false);
     firefoxOptions.setCapability("acceptInsecureCerts", true);
     firefoxOptions.setCapability("pageLoadStrategy", "normal");
+    firefoxOptions.setCapability("moz:debuggerAddress", false);
     firefoxOptions.setCapability("moz:firefoxOptions", Map.of(
             "prefs", Map.of(
                     "layout.css.scroll-behavior.spring-constant", "0", // Отключает плавную прокрутку
